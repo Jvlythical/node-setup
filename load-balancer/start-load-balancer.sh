473b91e696916d15f34fdecc3277ceba660255b2
Michael Yen <lvlichael@gmail.com>
2017-03-11 02:13:27 -0800

    Update load-balancer

--- load-balancer/start-load-balancer.sh
+++ load-balancer/start-load-balancer.sh
@@ -20,11 +20,13 @@ name=$CDE_NODE_NAMESPACE-load-balancer
 
 docker run -d -p $CDE_NODE_PORT:80 --name $name \
 -e "VIRTUAL_HOST=$CDE_NODE_HOST"  \
--e "LETSENCRYPT_HOST=$CDE_NODE_HOST" \
--e "LETSENCRYPT_EMAIL=$CDE_NODE_OWNER" \
 -v $(pwd)/default.conf:/etc/nginx/conf.d/default.conf \
 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf \
 nginx
+
+# Add below if using letsencrypt for ssl
+#-e "LETSENCRYPT_HOST=$CDE_NODE_HOST" \
+#-e "LETSENCRYPT_EMAIL=$CDE_NODE_OWNER" \
 
 #docker exec $name mkdir /usr/share/nginx/html/public
 #docker exec $name certbot certonly --email jvlarble@gmail.com --agree-tos --text --non-interactive --webroot  -w /usr/share/nginx/html/public -d dev.kodethon.com
