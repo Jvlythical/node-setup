83f03f3a10b489263c7ac9d751d83171b4b3b229
Michael Yen <lvlichael8@Gmail.Com>
2017-03-30 04:46:39 -0700

    Added NO_HTTPS arg to start-sentinel.sh

--- sentinel/start-sentinel.sh
+++ sentinel/start-sentinel.sh
@@ -73,19 +73,35 @@ mkdir logs 2> /dev/null
 touch "logs/$name.log"
 
 # Let's go
-docker run -itd  -h "$(uname -n)" --name $name \
---link $CDE_NODE_NAMESPACE-cache:memcache \
--v $host_drives_root:$container_drives_root \
--v $host_system_root:$container_system_root \
--v $(pwd)/../app/settings.yml:$rails_root/config/settings.yml \
--v $docker:/var/run/docker.sock -v $(pwd)/logs/$name.log:$rails_root/log/production.log \
--v /usr/lib/x86_64-linux-gnu/libapparmor.so.1:/usr/lib/x86_64-linux-gnu/libapparmor.so.1 \
--e "HOST_IP_ADDR=$CDE_NODE_HOST" \
--e "HOST_SYSTEM_ROOT=$host_system_root" -e "HOST_DRIVES_ROOT=$host_drives_root" \
--e "HOST_PORT=$http_port" -e "GROUP_PASSWORD=$CDE_GROUP_PASSWORD" \
--e "MASTER_IP_ADDR=$master_ip_addr" -e "MASTER_PORT=$master_port" \
--e "SELF_SYSTEM_ROOT=$container_system_root" -e "SELF_DRIVES_ROOT=$container_drives_root" \
-jvlythical/cde-sentinel:v0.1.0 sh -c "groupadd $docker_group -g $docker_gid; usermod -aG $docker_group www-data; bundle exec whenever -w; service cron restart; /bin/bash"
+if [ -z $1 ]; then
+	docker run -itd  -h "$(uname -n)" --name $name \
+	--link $CDE_NODE_NAMESPACE-cache:memcache \
+	-v $host_drives_root:$container_drives_root \
+	-v $host_system_root:$container_system_root \
+	-v $(pwd)/../app/settings.yml:$rails_root/config/settings.yml \
+	-v $docker:/var/run/docker.sock -v $(pwd)/logs/$name.log:$rails_root/log/production.log \
+	-v /usr/lib/x86_64-linux-gnu/libapparmor.so.1:/usr/lib/x86_64-linux-gnu/libapparmor.so.1 \
+	-e "HOST_IP_ADDR=$CDE_NODE_HOST" \
+	-e "HOST_SYSTEM_ROOT=$host_system_root" -e "HOST_DRIVES_ROOT=$host_drives_root" \
+	-e "HOST_PORT=$http_port" -e "GROUP_PASSWORD=$CDE_GROUP_PASSWORD" \
+	-e "MASTER_IP_ADDR=$master_ip_addr" -e "MASTER_PORT=$master_port" \
+	-e "SELF_SYSTEM_ROOT=$container_system_root" -e "SELF_DRIVES_ROOT=$container_drives_root" \
+	jvlythical/cde-sentinel:v0.1.0 sh -c "groupadd $docker_group -g $docker_gid; usermod -aG $docker_group www-data; bundle exec whenever -w; service cron restart; /bin/bash"
+else
+	docker run -itd  -h "$(uname -n)" --name $name \
+	--link $CDE_NODE_NAMESPACE-cache:memcache \
+	-v $host_drives_root:$container_drives_root \
+	-v $host_system_root:$container_system_root \
+	-v $(pwd)/../app/settings.yml:$rails_root/config/settings.yml \
+	-v $docker:/var/run/docker.sock -v $(pwd)/logs/$name.log:$rails_root/log/production.log \
+	-v /usr/lib/x86_64-linux-gnu/libapparmor.so.1:/usr/lib/x86_64-linux-gnu/libapparmor.so.1 \
+	-e "HOST_IP_ADDR=$CDE_NODE_HOST" -e "NO_HTTPS=true" \
+	-e "HOST_SYSTEM_ROOT=$host_system_root" -e "HOST_DRIVES_ROOT=$host_drives_root" \
+	-e "HOST_PORT=$http_port" -e "GROUP_PASSWORD=$CDE_GROUP_PASSWORD" \
+	-e "MASTER_IP_ADDR=$master_ip_addr" -e "MASTER_PORT=$master_port" \
+	-e "SELF_SYSTEM_ROOT=$container_system_root" -e "SELF_DRIVES_ROOT=$container_drives_root" \
+	jvlythical/cde-sentinel:v0.1.0 sh -c "groupadd $docker_group -g $docker_gid; usermod -aG $docker_group www-data; bundle exec whenever -w; service cron restart; /bin/bash"
+fi
 
 # cd /usr/share/nginx/html; export SECRET_KEY_BASE=\$(RAILS_ENV=production rake secret);
 #(nohup /sbin/my_init &);
