3d87c39c9685a60d0b7fefa2a07185dccef6c98b
Michael Yen <lvlichael8@gmail.com>
2017-08-30 15:32:58 -0700

    Change reverse proxy max client body size to 25mb

--- reverse-proxy/start-reverse-proxy.sh
+++ reverse-proxy/start-reverse-proxy.sh
@@ -3,6 +3,7 @@ docker rm -f letsencrypt
 
 docker run -d -p 80:80 -p 443:443 \
 	--name nginx-proxy \
+	-v $(pwd)/my_proxy.conf:/etc/nginx/conf.d/my_proxy.conf:ro \
 	-v $(pwd)/certs:/etc/nginx/certs:ro \
 	-v /etc/nginx/vhost.d \
 	-v /usr/share/nginx/html \
