28ea15c2c0195e1d13152691726e49bdc9cc6371
Michael Yen <lvlichael8@gmail.com>
2017-09-28 01:33:29 -0700

    Update zfs-migrate-drive.sh

--- utils/zfs-migrate-drives.sh
+++ utils/zfs-migrate-drives.sh
@@ -1,10 +1,16 @@
 cd ..
+
 mv drives drives.bak
-ls drives.bak | xargs -I{} sudo zfs create kodethon/production/drives/{}
-ls drives.bak | xargs -I{} sudo zfs set quota=10G kodethon/production/drives/{}
+zfs create kodethon/production/drives
 
 cd drives.bak
 for f in *; do
+	echo "Creating zfs dataset kodethon/production/drives/$f"
+	zfs create kodethon/production/drives/$f
+
+	echo "Setting 10G quota for zfs dataset kodethon/production/drives/$f"
+	zfs set quota=10G kodethon/production/drives/$f
+
 	for v in $f/*; do
 		echo "Moving $v to ../drives/$f"
 		mv $v ../drives/$f
