bcd7fa1820e98b3e52fb120443998e91d4203b2f
Michael Yen <lvlichael8@gmail.com>
2017-10-15 14:53:34 -0700

    Update node version to 0.9.10-rc

--- revive.sh
+++ revive.sh
@@ -3,7 +3,7 @@ docker start CDE-node-1
 docker start CDE-node-2
 docker start CDE-load-balancer
 docker start CDE-sentinel
-docker start CDE-backup
+#docker start CDE-backup
 docker start nginx-proxy
 docker start CDE-sentinel
 sh migrate.sh
