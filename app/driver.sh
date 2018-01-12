086556e079166b870b0752608896574c1d61636c
Martin Velez <marvelez@ucdavis.edu>
2018-01-12 04:05:45 -0800

    migrate.sh should take in an argument specifying the fully qualified docker image name of the cde-node.  So should app/driver.sh.  app/start-node-pro.sh should take this as a second argument.

--- app/driver.sh
+++ app/driver.sh
@@ -1,4 +1,7 @@
 # Export ENV variables
 export $(sed -e 's/:[^:\/\/]/=/g;s/$//g;s/ *=/=/g' ../env.yml)
 
-sh ./start-node-pro.sh $1
+# $1 is the number of nodes.
+# $2 is the fully qualified docker image name of the cde-node
+# In migrating, it is passed by the migrate.sh script.
+sh ./start-node-pro.sh $1 $2
