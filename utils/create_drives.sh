28976e47262769923c904b7f578295ac6028b3df
Michael Yen <lvlichael8@Gmail.Com>
2018-03-14 01:21:31 -0700

    Update create_drives.sh

--- utils/create_drives.sh
+++ utils/create_drives.sh
@@ -1,2 +1,5 @@
+zfs create -o mountpoint=/home/kodethon/production kodethon/production
+zfs create kodethon/production/drives
+zfs create kodethon/production/system
 cat drives.txt | xargs -i zfs create kodethon/production/drives/{}
 cat drives.txt | xargs -i zfs create kodethon/production/system/{}
