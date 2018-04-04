36ee604f763520213cf4c15e7817639a0b516174
Michael Yen <lvlichael8@gmail.com>
2018-04-03 20:06:30 -0700

    Update revive.sh

--- revive.sh
+++ revive.sh
@@ -1,9 +1,9 @@
+docker start CDE-rabbitmq
 docker start CDE-cache
 docker start CDE-node-1
 docker start CDE-node-2
 docker start CDE-load-balancer
-docker start CDE-sentinel
+#docker start CDE-sentinel
 #docker start CDE-backup
 docker start nginx-proxy
-docker start CDE-sentinel
 sh migrate.sh
