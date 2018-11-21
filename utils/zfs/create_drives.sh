0ee0f9007540fab94737208b92ce308bddc42a8e
Michael Yen <lvlichael8@gmail.com>
2018-11-21 11:59:01 +0000

    Update utils/zfs/create_drives.sh to chown created drives

--- utils/zfs/create_drives.sh
+++ utils/zfs/create_drives.sh
@@ -3,3 +3,6 @@ zfs create kodethon/production/drives
 zfs create kodethon/production/system
 cat drives.txt | xargs -i zfs create kodethon/production/drives/{}
 cat drives.txt | xargs -i zfs create kodethon/production/system/{}
+
+chown www-data:www-data /home/kodethon/production/drives/*
+chown www-data:www-data /home/kodethon/production/system/*
