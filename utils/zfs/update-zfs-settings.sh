189d940f29dc2758c540e3170dd9745b7b3f68b9
Michael Yen <lvlichael8@gmail.com>
2019-05-13 10:30:36 -0700

    Update zfs setup settings

--- utils/zfs/update-zfs-settings.sh
+++ utils/zfs/update-zfs-settings.sh
@@ -5,17 +5,22 @@
 # To make these changes permanent, it is recommended that you:
 # 1. Create the file /etc/modprobe.d/zfs.conf.
 # 2. update initramfs
-# 3. Reboot?
 #
 # Run this script as sudo.
 
-# Change the IO Scheduler.
-# This ensures that the cgroup block weight settings are enforced.
-printf 'noop' | sudo tee /sys/module/zfs/parameters/zfs_vdev_scheduler
-cat /sys/module/zfs/parameters/zfs_vdev_scheduler
-
 # Adjust IO scheduler
-printf 'noop' | sudo tee /sys/block/<device>/queue/scheduler
+printf 'noop' | sudo tee /sys/module/zfs/parameters/zfs_vdev_scheduler
+
+# This should be auto-adjusted by ZFS?
+#printf 'noop' | sudo tee /sys/block/<device>/queue/scheduler
+
+# Adjust ARC size while running
+echo "2147483648" | sudo tee /sys/module/zfs/parameters/zfs_arc_max
+echo "536870912" | sudo tee /sys/module/zfs/parameters/zfs_arc_min
+
+# Add optimizations
+echo "1" | sudo tee /sys/module/zfs/parameters/zfs_prefetch_disable
+echo "0" | sudo tee /sys/module/zfs/parameters/zfs_per_txg_dirty_frees_percent
 
 # Make changes permanent
 if [ ! -f "zfs.conf" ]; then
@@ -35,7 +40,4 @@ if [ ! -f "/etc/modprobe.d/zfs.conf" ]; then
 fi
 chown root:root /etc/modprobe.d/zfs.conf
 
-# Adjust it while running
-echo "2147483648" | sudo tee /sys/module/zfs/parameters/zfs_arc_max
-echo "536870912" | sudo tee /sys/module/zfs/parameters/zfs_arc_min
 update-initramfs -u -k all
